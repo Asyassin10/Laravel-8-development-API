e4022511ff775f5015ab07275f7e13355f126f94
Asyassin10 <asyassin06@gmail.com>
2021-08-24 18:47:36 +0100

    first commit

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contact = Contact::all();
+        return response()->json($this->arrayy($contact));
+    }
+    public function arrayy($contact)
+    {
+        foreach ($contact as $item) {
+            $data['name'] = $item->name;
+            $data['tele'] = $item->tele;
+        }
+        return $data;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'tele' => 'required',
+
+        ]);
+        $contact = Contact::create([
+            'name' => $request->name,
+            'tele' => $request->tele,
+
+        ]);
+        return response()->json($contact);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        $contact = Contact::all();
+        return response()->json($this->arrayy($contact));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'tele' => 'required',
+
+        ]);
+        $contact->update([
+            'name' => $request->name,
+            'tele' => $request->tele,
+
+        ]);
+        return response()->json($contact);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return response()->json(['message' => 'delete done']);
+    }
+}
